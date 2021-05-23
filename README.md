2fa778571f581736a43331382e0b75b7f4a5a769
eighty88 <49605393+eighty88@users.noreply.github.com>
2021-05-23 15:04:18 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+![Logo](https://raw.githubusercontent.com/Palpunte-Union/Default/master/resources/icon.png)
+# Nearby Chat
+ 近くにいないとチャットできないだけのあれ
+
+## Caution!!
+  - 作成者 : eight_y_88
+    - Twitter : [@Palpunte_Union](https://twitter.com/Palpunte_Union)
+  - 配布元URL : [https://github.com/Palpunte-Union/Nearby-Chat](https://github.com/Palpunte-Union/Nearby-Chat)
+
+## How To Build
+ - 基本的にReleaseから落としてください。
---
+++ src/main/java/com/github/eighty88/nearbych/NearbyChat.java
@@ -0,0 +1,28 @@
+package com.github.eighty88.nearbych;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NearbyChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(this, () -> Bukkit.getServer().getPluginManager().registerEvents(new Listener() {
+            @EventHandler
+            public void onChat(AsyncPlayerChatEvent e) {
+                String msg = e.getMessage();
+                for(Entity entity: e.getPlayer().getWorld().getNearbyEntities(e.getPlayer().getLocation(), 16, 16, 16)) {
+                    if(entity instanceof Player) {
+                        entity.sendMessage(msg);
+                    }
+                }
+                e.setCancelled(true);
+            }
+        }, this), 20L);
+    }
+}
